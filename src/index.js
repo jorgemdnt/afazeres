6dc272af3aa4e6924078f452ad7a256baf319ba5
Jorge Modesto <jorgemodes@gmail.com>
2019-05-13 00:15:25 -0300

    Added redux extension to middleware

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 import thunk from 'redux-thunk'
 import './index.css'
 import App from './components/App/'
 
+const reduxExtension = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk)
+  compose(applyMiddleware(thunk), reduxExtension),
 )
 
 ReactDOM.render(
